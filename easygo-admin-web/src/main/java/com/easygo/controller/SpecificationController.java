6c12e17a854a34f6527013024426d7f1972a7624
hudeng <76157259@qq.com>
2020-08-23 21:35:33 +0800

    增加规格的页面动态增加效果。

--- easygo-admin-web/src/main/java/com/easygo/controller/SpecificationController.java
+++ easygo-admin-web/src/main/java/com/easygo/controller/SpecificationController.java
@@ -2,6 +2,7 @@ package com.easygo.controller;
 
 import com.easygo.api.SpecificationClient;
 import com.easygo.pojo.Specification;
+import com.easygo.utils.MessageResults;
 import com.easygo.utils.PageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -10,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
 
 /**
  * Author：胡灯
@@ -28,5 +31,16 @@ public class SpecificationController {
         model.addAttribute("pageUtils",pageUtils);
         model.addAttribute("spec_name",spec_name);
         return "specification";
+    }
+
+    @RequestMapping("/specification_add")
+    @ResponseBody
+    public MessageResults specification_add(@RequestParam String spec_name,@RequestParam String[] option_name,@RequestParam String[] orders){
+        System.out.println("spec_name:"+spec_name);
+        System.out.println("option_name:"+ Arrays.toString(option_name));
+        System.out.println("orders:"+ Arrays.toString(orders));
+        MessageResults results = new MessageResults(200,"新增成功");
+        return results;
+
     }
 }
